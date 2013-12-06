d0e72bc590e094cfe5dbb0c45c44cda479f919dc
Nagesh Vanam <nag.501@gmail.com>
2013-12-06 23:33:44 +0530

    Gallery Click functionality

--- README.md
+++ README.md
@@ -1,8 +1,31 @@
-ajax-gallery
-============
-$(document).ready(function(){
-	$(".center").gallery($(".center"),{ url	: '/gallery',
-					    detailUrl : '/Images',
-					    loadCount : 15
-					    });
-});
+##AJAX GALLERY
+
+##SCRIPT
+```
+<script type="text/javascript" src="/plugins/jquery.ajax-gallery.js"></script>
+
+<script type="text/javascript">
+
+	$(document).ready(function() {
+		$(".center").gallery($(".center"), {
+			url : '/gallery',
+			detailUrl : '/Images',
+			loadCount : 24,
+			onSelect : function(url){
+				alert(url);
+			}
+		});
+	});
+	
+</script>
+```
+
+##HTML
+```
+<div class="center">
+		<div class="gallery"></div>
+		<div class="loader">
+			<img src="/ajax-loader.gif" />
+		</div>
+	</div>
+```
--- jquery.ajax-gallery.js
+++ jquery.ajax-gallery.js
@@ -1,41 +1,78 @@
-(function($){
-
+(function($) {
 	var ids = [];
 	var displayIndex = 0;
 	var settings = {};
-
-	$.fn.gallery = function(container, options){
-		settings = $.extend({loadCount:10, detailUrl:''}, options );
-		if(settings.detailUrl == ''){
+	// var loader;
+	var isLoading = false;
+	
+	$.fn.gallery = function(container, options) {
+		settings = $.extend({
+			loadCount : 10,
+			detailUrl : '',
+			onSelect : function(url){
+			}
+		}, options);
+		if (settings.detailUrl == '') {
 			settings.detailUrl = settings.url;
 		}
+
+		$(".loader img", container).css("visibility","hidden");
 		$.ajax({
-			url:settings.url,
-			method:"GET",
-			success:function(data){
+			url : settings.url,
+			method : "GET",
+			success : function(data) {
 				ids = data.ids;
-				load(container);
+				load($(".gallery",container));
 			}
 		});
-		
+
 		$(window).scroll(function() {
-		   var height = $('body').height();
-		   var scrollHeight = $(document).height();
-		   var st = $('body').scrollTop();
-		   if(st+20 >= scrollHeight - height){
-			   if(displayIndex + settings.loadCount <= ids.length)
-				load(container);
-		   }
+			var height = $('body').height();
+			var scrollHeight = $(document).height();
+			var st = $('body').scrollTop();
+			if (st + 20 >= scrollHeight - height && isLoading == false) {
+				$(".loader img", container).css("visibility","hidden");
+				if (displayIndex <= ids.length ) {  // + settings.loadCount
+					$(".loader img", container).css("visibility","visible");
+					isLoading = true;
+					load($(".gallery",container));
+				}
+			}
 		});
 	}
+	function load(container) {
+		var ind = (displayIndex + settings.loadCount) > ids.length ? ids.length : (displayIndex + settings.loadCount);
+		var img = new Array()
+		var loadCount = 0;
+		var j=0;
 
-	function load(container)
-	{
-		  var ind = displayIndex + settings.loadCount;
-		  for ( var i = displayIndex; i < ind; i++ ) {
-				displayIndex = i+1;
-				$(container).append('<div class="display-item"><img src="'+settings.detailUrl+'/'+ids[displayIndex]+'"></img></div>');
-		  }
+		for ( var i = displayIndex; i < ind; i++) {
+			img[j] = new Image();
+			img[j].onload = function() {
+				loaded();
+			};
+			img[j].onerror = function() {
+				loaded();
+			};
+			img[j].src = settings.detailUrl + '/' + ids[displayIndex];
+			displayIndex = i + 1;
+			j++;
+		}
+		function loaded() {
+			loadCount++;
+			if (loadCount == img.length) {
+				$(".loader img", container).css("visibility","hidden");
+				for ( var i = 0; i < img.length; i++) {
+					$(container).append(
+							'<div class="display-item"><img src="' + img[i].src
+									+ '"></img></div>');
+					
+				}
+				$(".display-item img",container).click(function(){
+					settings.onSelect($(this).attr("src"));
+				});
+				isLoading = false;
+			}
+		}
 	}
-
 }(jQuery));
